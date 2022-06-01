9476f8e32159cb0c048fac267784d46bd24a6bb5
Huan Maverick <simbioza1998@gmail.com>
2022-06-01 04:11:40 +0200

    Add project files.

---
+++ DogShelterWeb/Controllers/DogRecordController.cs
@@ -0,0 +1,23 @@
+﻿using DogShelterWeb.Data;
+using DogShelterWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogShelterWeb.Controllers
+{
+    public class DogRecordController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DogRecordController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objDogRecordList=_db.Categories;
+            return View(objDogRecordList);
+        }
+    }
+}
---
+++ DogShelterWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DogShelterWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogShelterWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
